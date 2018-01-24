53c5d43e7b91ad4757e1313963864e79566b3979
gerry.zhong1 <qiang.zhong02@ele.me>
2018-01-24 15:59:35 +0800

    0.0.2版本发布。增加demo案例

--- js-common/SSE.js
+++ js-common/SSE.js
@@ -5,160 +5,176 @@
  * date:         2017-10-15
  * email:        gerry.zhong@outlook.com
  * update:          --1.1   提供通用初始化解决方案
+ *                  --1.2   完善基础配置和设计，包括容错机制，增加捕获err的时候关闭http通道
  */
-(function (window) {
-    var initParam = {
-        url :'',                                //所链接的服务器地址
-        data:'',                                //所发送的客户端数据
-        customEvent:[],                         //自定义事件 格式：[{name:'事件名称',callback:function(res){}}]
-        withCredentials:false,                 //是否发送跨域凭证
-        serverTimeout:60000,                    //服务器http默认超时时间   带考虑：客户端配置服务器时间，不安全
-        clientConnection:3000,                  //设置浏览器重连时间，浏览器默认3s重连，
-        openEvent:function () {},              //客户端开始链接的事件
-        messageEvent:function () {},           //客户端接受到消息的事件（如不自定义系统默认）
-        errorEvent:function () {}              //客户端错误事件
-    }
-    //初始化参数固定类型检查
-    var initParamType = {
-        url :'String',
-        data:'Object',
-        customEvent:'Array',
-        withCredentials:'Boolean',
-        serverTimeout:'Number',
-        clientConnection:'Number',
-        openEvent:'function',
-        messageEvent:'function',
-        errorEvent:'function'
-    };
+(function () {
+  // 初始化参数
+  var initParam = {
+    url :'',                                //所链接的服务器地址
+    data:'',                                //所发送的客户端数据
+    customEvent:[],                         //自定义事件 格式：[{name:'事件名称',callback:function(res){}}]
+    withCredentials:false,                 //是否发送跨域凭证
+    serverTimeout:60000,                    //服务器http默认超时时间   带考虑：客户端配置服务器时间，不安全
+    clientConnection:3000,                  //设置浏览器重连时间，浏览器默认3s重连，
+    openEvent:function () {},              //客户端开始链接的事件
+    messageEvent:function () {},           //客户端接受到消息的事件（如不自定义系统默认）
+    errorEvent:function () {}              //客户端错误事件
+  }
+  // 初始化参数固定类型检查
+  var initParamType = {
+    url :'String',
+    data:'Object',
+    customEvent:'Array',
+    withCredentials:'Boolean',
+    serverTimeout:'Number',
+    clientConnection:'Number',
+    openEvent:'function',
+    messageEvent:'function',
+    errorEvent:'function'
+  };
 
-    // 依赖的工具类型
-    var tool = {
-        isSupported:function () {
-            if (!EventSource) throw new Error("sorry,your browser does't support this property")
-        },
-        //类型判断
-        is: (function checkType() {
-            var is = {
-                types: ["Array", "Boolean", "Date", "Number", "Object", "RegExp", "String", "Window", "HTMLDocument", "function", "FormData"]
-            };
-            for (var i = 0, c; c = is.types[i++];) {
-                is[c] = (function (type) {
-                    return function (obj) {
-                        var temp;
-                        if (type === "function") {
-                            temp = typeof obj == type
-                        } else {
-                            temp = Object.prototype.toString.call(obj) == "[object " + type + "]";
-                        }
-                        return temp;
-                    }
-                })(c);
-            }
-            ;
-            return is;
-        })(),
-        // 判断是否是当前自身属性
-        hasOwn: function (obj, key) {
-            return Object.prototype.hasOwnProperty.call(obj, key)
-        },
-        //获取对象的key
-        keys: function (obj) {
-            if (Object.keys) return Object.keys(obj);
+  var root = this
 
-            var keys = [];
-            for (var key in obj) {
-                if (this.hasOwn(obj, key)) keys.push(key);
-            }
-            ;
-            return keys
-        },
-        //each循环
-        each: function (obj, callback) {
-            var keys = this.keys(obj);
-            if (this.is.Array(obj) && [].forEach) {  //判断是否为数组且支持新特性
-                obj.forEach(callback);
+  // 依赖的工具类型
+  var tool = {
+    // 是否支持推送技术
+    isSupported:function () {
+      if (!EventSource) throw new Error("sorry,your browser does't support this property")
+    },
+    //类型判断
+    is: (function checkType() {
+      var is = {
+        types: ["Array", "Boolean", "Date", "Number", "Object", "RegExp", "String", "Window", "HTMLDocument", "function", "FormData"]
+      };
+      for (var i = 0, c; c = is.types[i++];) {
+        is[c] = (function (type) {
+          return function (obj) {
+            var temp;
+            if (type === "function") {
+              temp = typeof obj == type
             } else {
-                var i = 0, len = keys.length, key, item;
-                while (i < len) {
-                    key = keys[i++];
-                    item = obj[key];
-                    callback.call(obj, item, key);
-                }
+              temp = Object.prototype.toString.call(obj) == "[object " + type + "]";
             }
-        },
-        // 合并对象
-        MergeObject: function (target, source) {
-            if (Object.assign) {
-                return Object.assign(target, source)
-            }
-            var sourceKeys = this.keys(source),
-                i = 0
-            var len = sourceKeys.length;
-            while (i < len) {
-                var key = sourceKeys[i++]
-                target[key] = source[key];
-            }
-            return target;
-        },
-        // 初始化参数
-        initParam:function (options) {
-            var temp = {};
-            tool.MergeObject(temp, initParam);
-            //解决深度拷贝引用地址问题
-            temp.data = JSON.parse(JSON.stringify(temp.data))
-            tool.MergeObject(temp, options);
-            tool.checkDataTypeBatch(temp, initParamType);
             return temp;
-        },
-        //批量检查数据类型和修正
-        checkDataTypeBatch: function (obj, objType) {
-            tool.each(obj, function (value, key) {
-                var typeName = objType[key];
-                if (!tool.is[typeName](value)) {
-                    obj[key] = initParam[key]
-                }
-            })
-        },
-    }
+          }
+        })(c);
+      }
+      ;
+      return is;
+    })(),
+    // 判断是否是当前自身属性
+    hasOwn: function (obj, key) {
+      return Object.prototype.hasOwnProperty.call(obj, key)
+    },
+    //获取对象的key
+    keys: function (obj) {
+      if (Object.keys) return Object.keys(obj);
 
-    // 抛出对象
-    var output = {
-        create:function (options) {
-            var param = tool.initParam(options),sendData = '';
-
-            if (param.data){
-                tool.each(param.data, function (item, index) {
-                    sendData += (index + "=" + item + "&")
-                });
-                sendData = sendData.slice(0, -1);
-            }
-
-            var es = new EventSource(param.url+'?'+sendData);
-
-            es.addEventListener('open',function (e) {
-                param.openEvent(e)
-            });
-
-            es.addEventListener('message',function (e) {
-                param.messageEvent(e)
-            });
-
-            es.addEventListener('error',function (e) {
-                param.errorEvent(e)
-            });
-
-            // 创建用户自定义事件
-            if (param.customEvent.length > 0){
-                tool.each(param.customEvent,function (item) {
-                    es.addEventListener(item.name,item.callback);
-                })
-            }
+      var keys = [];
+      for (var key in obj) {
+        if (this.hasOwn(obj, key)) keys.push(key);
+      }
+      ;
+      return keys
+    },
+    //each循环
+    each: function (obj, callback) {
+      var keys = this.keys(obj);
+      if (this.is.Array(obj) && [].forEach) {  //判断是否为数组且支持新特性
+        obj.forEach(callback);
+      } else {
+        var i = 0, len = keys.length, key, item;
+        while (i < len) {
+          key = keys[i++];
+          item = obj[key];
+          callback.call(obj, item, key);
         }
-    }
+      }
+    },
+    // 合并对象
+    MergeObject: function (target, source) {
+      if (Object.assign) {
+        return Object.assign(target, source)
+      }
+      var sourceKeys = this.keys(source),
+        i = 0
+      var len = sourceKeys.length;
+      while (i < len) {
+        var key = sourceKeys[i++]
+        target[key] = source[key];
+      }
+      return target;
+    },
+    // 初始化参数
+    initParam:function (options) {
+      var temp = {};
+      tool.MergeObject(temp, initParam);
+      //解决深度拷贝引用地址问题
+      temp.data = JSON.parse(JSON.stringify(temp.data))
+      tool.MergeObject(temp, options);
+      tool.checkDataTypeBatch(temp, initParamType);
+      return temp;
+    },
+    //批量检查数据类型和修正
+    checkDataTypeBatch: function (obj, objType) {
+      tool.each(obj, function (value, key) {
+        var typeName = objType[key];
+        if (!tool.is[typeName](value)) {
+          obj[key] = initParam[key]
+        }
+      })
+    },
+  }
 
-    // 注册全局的对象
-    window.SSE = new (function () {
-        tool.isSupported()    //检测版本
-        return output;
-    })
-})(this)
+  // 抛出对象
+  var output = {
+    create:function (options) {
+      var param = tool.initParam(options),sendData = '';
+
+      if (param.data){
+        tool.each(param.data, function (item, index) {
+          sendData += (index + "=" + item + "&")
+        });
+        sendData = sendData.slice(0, -1);
+      }
+
+      var es = new EventSource(param.url+'?'+sendData);
+
+      es.addEventListener('open',function (e) {
+        param.openEvent(e)
+      });
+
+      es.addEventListener('message',function (e) {
+        param.messageEvent(e)
+      });
+
+      es.addEventListener('error',function (e) {
+        param.errorEvent(e)
+        es.close()  // 关闭连接
+      });
+
+      // 创建用户自定义事件
+      if (param.customEvent.length > 0){
+        tool.each(param.customEvent,function (item) {
+          es.addEventListener(item.name,item.callback);
+        })
+      }
+    }
+  }
+
+  function init(){
+    // 检测版本
+    tool.isSupported();
+    if (root === window){
+      root.SSE = output
+    }else{
+      // 将抛出对象注册到exports上
+      tool.each(output,function (value, key) {
+        root[key] = value;
+      });
+    }
+  };
+
+  init()
+
+}.call(this));
--- js-node/SSE-server.js
+++ js-node/SSE-server.js
@@ -1,23 +1,140 @@
 var http = require("http");
 var url = require('url');
+var qs = require('querystring');//解析参数的库
+var sendObject = {}, count = 0;
+var gerry = {}
 
+// 向除自己的所有人推送信息
+function sendAll(data) {
+  for (var index in sendObject) {
+    if (data.name !== index) {
+      sendObject[index].write("retry: " + data.retry + "\n");
+      if (data.event) {
+        sendObject[index].write("event: " + data.event + "\n");
+      }
+
+      var sengData = {
+        author: data.name,
+        data: data.data
+      }
+      sendObject[index].write("data: " + JSON.stringify(sengData) + "\n\n");
+    }
+  }
+}
+
+// 聊天系统的demo推送测试
 http.createServer(function (req, res) {
-    var arg1 = url.parse(req.url, true).query;
-    console.log(arg1.name);
-    // 设置跨域和响应
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-    res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    res.writeHead(200, {
-        "Content-Type": "text/event-stream",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive"
-    });
-    setInterval(function () {
-        res.write("id: 1\n");                                   //设置当前数据的唯一标识
-        res.write("event: message\n");                          //设置浏览器响应的函数
-        res.write("retry: 10000\n");                            //设置浏览器断线重连的时间
-        res.write("data:"+ new Date().toISOString() +"\n\n");    //设置当前数据的相应值
-    },3000)
 
-}).listen(8888);
+  var arg1 = url.parse(req.url, true).query;
+  console.log(arg1.name);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive"
+  });
+
+  res.setTimeout(5000, function () {
+    res.write(":this is test")
+  })
+  sendObject[arg1.name] = res;
+}).listen(8074);
+
+// ajax双工执行发送机制
+http.createServer(function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  res.writeHead(200, {
+    "content-type": "application/json; charset=utf-8"
+  });
+  var arr = []
+  new Promise(function (x, xx) {
+    req.on("data", function (data) {
+      arr.push(data);
+    });
+
+    req.on("end", function () {
+      var data = Buffer.concat(arr).toString(), ret;
+      try {
+        ret = JSON.parse(data);
+        x(ret)
+      } catch (err) {
+      }
+    })
+  }).then(function (req) {
+    var data = {
+      retry: 10000,
+      name: req.name,
+      data: req.message,
+      event: req.event
+    }
+    sendAll(data)
+    // res.write(Buffer.concat(arr).toString())
+
+  })
+  res.end()
+}).listen(8075)
+
+
+var cacheUpdate = {}
+
+// 缓存更新和线上正在使用的代码热修复和强制用户重新请求去拉取最新代码
+http.createServer(function (req, res) {
+
+  var arg1 = url.parse(req.url, true).query;
+  console.log(arg1.name);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive"
+  });
+
+  res.setTimeout(5000, function () {
+    res.write(":this is test")
+  })
+  cacheUpdate[arg1.name] = res;
+}).listen(8076);
+
+// 触发推送的动作
+http.createServer(function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  res.writeHead(200, {
+    "content-type": "application/json; charset=utf-8"
+  });
+  var arr = []
+  new Promise(function (x, xx) {
+    req.on("data", function (data) {
+      arr.push(data);
+    });
+
+    req.on("end", function () {
+      var data = Buffer.concat(arr).toString(), ret;
+      try {
+        ret = JSON.parse(data);
+        x(ret)
+      } catch (err) {
+      }
+    })
+  }).then(function (req) {
+    var data = {
+      retry: 10000,
+      name: req.name,
+      data: req.message,
+      event: req.event
+    }
+
+    cacheUpdate[data.name].write("retry: " + data.retry + "\n");
+    cacheUpdate[data.name].write("event: " + data.event + "\n");
+    cacheUpdate[data.name].write("data: " + data.data + "\n\n");
+
+  })
+  res.end()
+}).listen(8077)
